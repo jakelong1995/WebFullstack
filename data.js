6a796f51b555396878d1cd29356e6ed31aaa54c1
jakelong1995 <hoanglong2579@gmail.com>
2024-02-28 14:50:50 +0700

    commit

---
+++ data.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: 1,
+    userName: "john_doe",
+    email: "john.doe@example.com",
+    address: "123 Main Street, Cityville",
+    age: 28,
+  },
+  {
+    id: 2,
+    userName: "jane_smith",
+    email: "jane.smith@example.com",
+    address: "456 Oak Avenue, Townsville",
+    age: 35,
+  },
+];
+
+export default users;
