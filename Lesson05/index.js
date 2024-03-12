8c7a7238ff714ab98c7b9caa6b698d74d0330367
jakelong1995 <hoanglong2579@gmail.com>
2024-03-12 16:31:19 +0700

    12/3

--- Lesson05/index.js
+++ Lesson05/index.js
@@ -1,8 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/routes.js";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4001;
 const uri = `mongodb+srv://${process.env.MONGODB_DB_NAME}:${process.env.MONGODB_PWD}@cluster0.djglaud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
