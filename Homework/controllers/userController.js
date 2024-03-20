3fdb4d21831ff0248e073ffe58157fafd8fdc830
jakelong1995 <hoanglong2579@gmail.com>
2024-03-20 20:11:49 +0700

    jwt

--- Homework/controllers/userController.js
+++ Homework/controllers/userController.js
@@ -3,16 +3,24 @@ import User from "../models/userModel.js";
 const createUser = async (req, res) => {
   try {
      // Parse request body
-    const { userName } = req.body;
+    const { userName, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create a new user instance
     const newUser = new User({
       userId: generateUserId(),
-      userName
+      userName,
+      password: hashedPassword // Store the hashed password
     });
 
     // Save the user to the database
     const savedUser = await newUser.save();
+
+    const secretKey = process.env.SECRET_KEY;
+    // Generate JWT token
+    const token = jwt.sign({ userId: savedUser.userId }, secretKey, { expiresIn: '1h' });
 
     res.status(201).json({success: true, message: 'User created successfully', User: savedUser});
   } catch (error) {
--- Homework/models/userModel.js
+++ Homework/models/userModel.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
   userName: {
     type: String,
     required: true
+  },
+  password: {
+    type: String,
+    required: true
   }
 });
 
